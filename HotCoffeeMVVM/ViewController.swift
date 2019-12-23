226b2e402ae9ecd2d7824fa70de62e5019927eb2
Agnes <agneslo@koodata.com.tw>
2019-12-23 18:09:24 +0800

    Initial Commit

---
+++ HotCoffeeMVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HotCoffeeMVVM
+//
+//  Created by Agnes Lo on 2019/12/23.
+//  Copyright © 2019 Agnes Lo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
