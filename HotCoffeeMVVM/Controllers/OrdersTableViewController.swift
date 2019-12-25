f0f145563b1974c299131696d6e59fda4758517a
Agnes <agneslo@koodata.com.tw>
2019-12-25 18:20:30 +0800

    Get orders in OrdersTableViewController.

--- HotCoffeeMVVM/Controllers/OrdersTableViewController.swift
+++ HotCoffeeMVVM/Controllers/OrdersTableViewController.swift
@@ -11,5 +11,29 @@ import UIKit
 
 class OrdersTableViewController: UITableViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        populateOrders()
+    }
     
+    private func populateOrders() {
+        
+        guard let coffeeOrdersURL = URL(string: "https://guarded-retreat-82533.herokuapp.com/orders") else {
+            
+            fatalError("URL was incorrect")
+        }
+        
+        let resource = Resource<[Order]>(url: coffeeOrdersURL)
+        
+        Webservice().load(resource: resource, completion: { (result) in
+            
+            switch result {
+                
+            case .success(let orders): print(orders)
+                
+            case .failure(let error): print(error)
+            }
+        })
+    }
 }
