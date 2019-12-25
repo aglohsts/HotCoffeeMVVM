cf1e2ff2acea133722a95cfb493e470e2f9d28f6
Agnes <agneslo@koodata.com.tw>
2019-12-25 17:28:37 +0800

    Add AddOrderViewController and OrdersTableViewController.

---
+++ HotCoffeeMVVM/Controllers/AddOrderViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AddOrderViewController.swift
+//  HotCoffeeMVVM
+//
+//  Created by Agnes Lo on 2019/12/25.
+//  Copyright © 2019 Agnes Lo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddOrderViewController: UIViewController {
+    
+}
---
+++ HotCoffeeMVVM/Controllers/OrdersTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  OrdersTableViewController.swift
+//  HotCoffeeMVVM
+//
+//  Created by Agnes Lo on 2019/12/25.
+//  Copyright © 2019 Agnes Lo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class OrdersTableViewController: UITableViewController {
+    
+    
+}
