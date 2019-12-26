3ee61f17a5e56106cc05f9af32f6c8a0c12fa818
Agnes <agneslo@koodata.com.tw>
2019-12-26 12:02:27 +0800

    Add OrderListViewModel.

--- HotCoffeeMVVM/View Models/OrderViewModel.swift
+++ HotCoffeeMVVM/View Models/OrderViewModel.swift
@@ -8,6 +8,24 @@
 
 import Foundation
 
+class OrderListViewModel {
+    
+    var ordersViewModel: [OrderViewModel]
+    
+    init() {
+        
+        self.ordersViewModel = [OrderViewModel]()
+    }
+}
+
+extension OrderListViewModel {
+    
+    func orderViewModel(at index: Int) -> OrderViewModel {
+        
+        return self.ordersViewModel[index]
+    }
+}
+
 struct OrderViewModel {
     
     let order: Order
@@ -28,5 +46,10 @@ extension OrderViewModel {
     var type: String {
         
         return self.order.type.rawValue.capitalized
+    }
+    
+    var size: String {
+        
+        return self.order.size.rawValue.capitalized
     }
 }
